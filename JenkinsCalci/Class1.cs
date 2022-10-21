502404f10a4d9dbe0839fe9ee79b965f56ee8a52
saivignesh2001s <saivignesh2001s@gmail.com>
2022-10-21 09:38:28 +0530

    Add project files.

---
+++ JenkinsCalci/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JenkinsCalci
+{
+    public class Calci
+    {
+        public int Add(int i,int j)
+        {
+            return i + j;
+        }
+        public int subtract(int i,int j)
+        {
+            if (j > i)
+            {
+                return j - i;
+            }
+            else
+            {
+                return i - j;
+            }
+        }
+        public int multiply(int i, int j)
+        {
+            return i * j;
+        }
+    }
+
+}
